35f468c57e307e16391e0add3e1879e06568a033
Mark Austin <mark@markaustinonline.com>
2018-01-03 16:14:57 -0800

    Working site

--- app/controllers/kitties_controller.rb
+++ app/controllers/kitties_controller.rb
@@ -18,27 +18,58 @@ class KittiesController < ApplicationController
   def create
     @kitty = Kitty.new(kitty_params)    #  ORIGINAL CALL:: Kitty.new(params[:kitty]) ::: but use kitty_params for security measures
     
+    #### TRADITIONAL WAY #####
     # @kitty = Kitty.new
     # @kitty.name = params[:kitty][:name]
     # @kitty.age = params[:kitty][:age]
     # @kitty.breed = params[:kitty][:breed]
     # @kitty.image = params[:kitty][:image]
 
-    @kitty.save
+        if @kitty.save    #checks if CREATE was successfull (TRUE)
 
-    #obejct saved to DB, ID assigned, redirected to Show page
-    redirect_to "/kitties/#{@kitty.id}"
+          #obejct saved to DB, ID assigned, redirected to Show page
+          redirect_to "/kitties/#{@kitty.id}"
+        else
+          redirect_to "/kitties/new"    #if failed reutrn to NEW form 
+        end # end IF
 
   end
 
   def edit
+
+      @kitty = Kitty.find(params[:id])
+
   end
+
+
 
   def update
+
+    @kitty = Kitty.find(params[:id])
+
+    if @kitty.update(kitty_params)    #checks if UPDATE was successfull (returns TRUE)
+          #obejct saved to DB, redirected to Show page
+      redirect_to kitty_path(@kitty)   #a new way to redirect using ALIASES
+    else
+      redirect_to edit_kitty_path(@kitty)    #if failed reutrn to EDIT form 
+    end # end IF
   end
 
+
+  
   def destroy
+
+    @kitty = Kitty.find(params[:id])
+    
+    if @kitty.destroy
+      redirect_to kitties_path    #new paths using aliases
+    else
+      redirect_to kitty_path(@kitty)  #new paths using aliases
+    end
+
   end
+
+
 
   private    #FOR SECURITY 
   def kitty_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
 
  root 'kitties#index'    #<--- another way to establish home route. 
 
-  get '/kitties' => 'kitties#index'    #list all kitties
+  get '/kitties' => 'kitties#index', as: :kitties    #list all kitties
 
-  get '/kitties/new' => 'kitties#new'  #display a form to make a new kitty
+  get '/kitties/new' => 'kitties#new', as: :new_kitty   #display a form to make a new kitty
 
-  get '/kitties/:id' => 'kitties#show' #display one kitty
+  get '/kitties/:id' => 'kitties#show', as: :kitty  #display one kitty
 
   post '/kitties' => 'kitties#create'  #submit new kitty to database
 
-  get '/kitties/:id/edit' => 'kitties#edit'  #display a kitty to EDIT it
+  get '/kitties/:id/edit' => 'kitties#edit', as: :edit_kitty  #display a kitty to EDIT it
 
   patch '/kitties/:id' => 'kitties#update'  #after EDITING, u need an id when updating a kitty
 
